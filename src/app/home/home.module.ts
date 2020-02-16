6a35686d4f60a5206212532eec8c1ca4c5564cde
Freddy Tamwo <55187739+Donfreddy@users.noreply.github.com>
2020-02-16 15:48:00 +0100

    first commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,11 +6,14 @@ import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
 
+import { NgCalendarModule } from 'ionic2-calendar';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    NgCalendarModule,
     RouterModule.forChild([
       {
         path: '',
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,128 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit, Inject, LOCALE_ID } from '@angular/core';
+import { CalendarComponent } from 'ionic2-calendar/calendar';
+import { AlertController } from '@ionic/angular';
+import { formatDate } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  collapseCard: boolean;
+
+  event = {
+    title: '',
+    desc: '',
+    startTime: '',
+    endTime: '',
+    allDay: false
+  };
+
+  minDate = new Date().toISOString();
+  
+
+  eventSource = [];
+  viewTitle;
+
+  calendar = {
+    mode: 'month',
+    currentDate: new Date(),
+  };
+
+  @ViewChild(CalendarComponent, { static: false }) myCal: CalendarComponent;
+
+  constructor(private alertCtrl: AlertController, @Inject(LOCALE_ID) private locale: string) { }
+
+  ngOnInit() {
+    this.resetEvent();
+    this.collapseCard = true;
+  }
+
+  resetEvent() {
+    this.event = {
+      title: '',
+      desc: '',
+      startTime: new Date().toISOString(),
+      endTime: new Date().toISOString(),
+      allDay: false
+    };
+  }
+
+  // Créez le bon format d'événement et rechargez la source
+  addEvent() {
+    let eventCopy = {
+      title: this.event.title,
+      startTime: new Date(this.event.startTime),
+      endTime: new Date(this.event.endTime),
+      allDay: this.event.allDay,
+      desc: this.event.desc
+    }
+
+    if (eventCopy.allDay) {
+      let start = eventCopy.startTime;
+      let end = eventCopy.endTime;
+
+      eventCopy.startTime = new Date(Date.UTC(start.getUTCFullYear(), start.getUTCMonth(), start.getUTCDate()));
+      eventCopy.endTime = new Date(Date.UTC(end.getUTCFullYear(), end.getUTCMonth(), end.getUTCDate() + 1));
+    }
+
+    this.eventSource.push(eventCopy);
+    this.myCal.loadEvents();
+    this.resetEvent();
+
+    this.collapseCard = !this.collapseCard;
+  }
+
+  //Changer le mois / la semaine / le jour en cours
+  next() {
+    var swiper = document.querySelector('.swiper-container')['swiper'];
+    swiper.slideNext();
+  }
+
+  back() {
+    var swiper = document.querySelector('.swiper-container')['swiper'];
+    swiper.slidePrev();
+  }
+
+  // Changement entre mois / semaine / jour
+  changeMode(mode) {
+    this.calendar.mode = mode;
+  }
+
+  // Focus aujourd'hui
+  today() {
+    this.calendar.currentDate = new Date();
+  }
+
+  // Lorsque la plage de dates sélectionnée et le titre ont changé
+  onViewTitleChanged(title) {
+    this.viewTitle = title;
+  }
+
+  // Lorsqu'un événement de calendrier a été cliqué
+  async onEventSelected(event) {
+    // Utiliser un pipe de date angular pour la conversion
+    let start = formatDate(event.startTime, 'medium', this.locale);
+    let end = formatDate(event.endTime, 'medium', this.locale);
+
+    const alert = await this.alertCtrl.create({
+      header: event.title,
+      subHeader: event.desc,
+      message: 'De: ' + start + '<br><br>à: ' + end,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  // Lorsque la plage horaire a été cliquée
+  onTimeSelected(ev) {
+    let selected = new Date(ev.selectedTime);
+    this.event.startTime = selected.toISOString();
+    selected.setHours(selected.getHours() + 1);
+    this.event.endTime = (selected.toISOString());
+  }
 
 }
